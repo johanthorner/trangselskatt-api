1024ce8aff86c5829ba2de49c59bc3f1abe3134a
Johan Thörner <johan@thorner.se>
2021-01-24 16:40:53 +0100

    Added Method for checking if date is weekende taxfree month or day before weekend

--- Services/TaxCalculationService.cs
+++ Services/TaxCalculationService.cs
@@ -2,6 +2,8 @@ using TrangselskattAPI;
 using System.Linq;
 using System.Collections.Generic;
 using Microsoft.Extensions.Configuration;
+using System;
+
 public class TaxCalculationService : ITaxCalculationService
 {
     private readonly IConfiguration Configuration;
@@ -20,19 +22,28 @@ public class TaxCalculationService : ITaxCalculationService
             
         var result = OrderPassagesByDate(request); 
               
-        if(NonTaxPayingVehicle.Any(a => a == request.VehicleType)){
+        if(NonTaxPayingVehicle.Any(a => a == request.VehicleType))
+        {
             return result;
         }     
 
+        result = CalculateTaxPerDay(result);
+
         return result;
     } 
+
+    private TaxResult CalculateTaxPerDay(TaxResult result){
+       
+        return result;
+    }
 
     private TaxResult OrderPassagesByDate(TaxRequest request){
         
         var result = new TaxResult(); 
 
         result.Dates = request.PassagesThroughCustoms
-        .GroupBy(a => a.Date)
+        .Where(a => !DateIsTaxFree(a))
+        .GroupBy(a => a.Date)      
         .Select(a => 
         new DateOfPassage()
         {
@@ -42,5 +53,19 @@ public class TaxCalculationService : ITaxCalculationService
         .ToList();
 
         return result;
-    }    
+    }  
+
+    private bool DateIsTaxFree(DateTime date){
+
+        var isTaxFreeMonth = date.Month == 7;     
+        var isDayBeforeWeekend = (date.DayOfWeek + 1) == DayOfWeek.Saturday;
+        var isWeekend = date.DayOfWeek == DayOfWeek.Saturday || date.DayOfWeek == DayOfWeek.Saturday;
+        
+        if(isTaxFreeMonth|| isWeekend || isDayBeforeWeekend)
+        {
+            return true;
+        }
+
+        return false;
+    }
 }
